23e4a009f94502bf9acfc04e22b869b227bd010c
heysaleme <mrst.amiari@gmail.com>
2026-02-21 23:54:23 +0500

    imports added

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+
+	"lem-in/internal/formatter"
+	"lem-in/internal/graph"
+	"lem-in/internal/parser"
+	"lem-in/internal/simulation"
+	"lem-in/internal/solver"
+)
 
 func main() {
-	input := parser.Parse(os.Args[1])
-	graph := graph.Build(input)
-	paths := solver.FindOptimalPaths(graph)
-	moves := simulation.Run(paths, input.Ants)
-	formatter.Print(input, moves)
+	if len(os.Args) != 2 {
+		fmt.Println("ERROR: invalid data format")
+		return
+	}
+
+	farm, err := parser.Parse(os.Args[1])
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+
+	g := graph.Build(farm)
+
+	path, err := solver.BFS(g)
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+
+	moves := simulation.Run(path, farm.Ants)
+
+	formatter.Print(farm.RawLines, moves)
 }
